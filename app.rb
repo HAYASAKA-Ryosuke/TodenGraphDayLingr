c94c27118f39a6eb2d9f8cccdd82834050e1aba7
kuroneko konoha <hayasaka.ryosuke@gmail.com>
2014-01-01 00:36:59 +0900

    add diffmethod

--- app.rb
+++ app.rb
@@ -31,7 +31,17 @@ def filedownload(url,filename)
         system('nkf -w --overwrite '+ filename)
 end
 
-def todayfileanalysis(filename)
+def diff(h,y)
+	y0=y.pop
+	result =[]
+	y.each do |param|
+		result << (param-y0)/h
+		y0=param
+	end
+	return result
+end
+
+def todayfileanalysis(filename,option)
         if((DateTime.now.to_time-File.stat("./data/juyo-j.csv").mtime.to_time).to_i > 0) then
                 filedownload('http://www.tepco.co.jp/forecast/html/images/juyo-j.csv','./data/juyo-j.csv')
                 powerdata=[]
@@ -47,14 +57,16 @@ def todayfileanalysis(filename)
                         count=count+1
                 end
         end
-
+	if option != nil
+		powerdata=diff(1,powerdata)
+	end
         xlabels={0 => '0',12 => '1',24 => '2',36 => '3',48 => '4',60 => '5',72 => '6',84 => '7',96 => '8',108 => '9',120 => '10',132 => '11',144 => '12',156 => '13',168 => '14',180 => '15',192 => '16',204 => '17',216 => '18',228 => '19',240 => '20',252 => '21',264 => '22',276 => '23'}
         graphgenerate(xlabels,powerdata,'Hour','Power Usage[MW]',filename)
         return ""
 
 end
 
-def pastfileanalysis(dateyear,datemonth,dateday)
+def pastfileanalysis(dateyear,datemonth,dateday,option)
         filename=dateyear.to_s+'-'+datemonth.to_s+'-'+dateday.to_s+'.json'
         if(File.exist?("./data/"+filename)==false) then
                 url='http://tepco-usage-api.appspot.com/'+dateyear.to_s+'/'+datemonth.to_s+'/'+dateday.to_s+'.json'
@@ -65,6 +77,9 @@ def pastfileanalysis(dateyear,datemonth,dateday)
                 jsonpowerdata.each do |data|
                         powerdata << data['usage'].to_f*10000/10**6#kWはわかりにくいのでMW
                 end
+		if option != nil
+			powerdata=diff(1,powerdata)
+		end
                 xlabels={0 => '0',1 => '1',2 => '2',3 => '3',4 => '4',5 => '5',6 => '6',7 => '7',8 => '8',9 => '9',10 => '10',11 => '11',12 => '12',13 => '13',14 => '14',15 => '15',16 => '16',17 => '17',18 => '18',19 => '19',20 => '20',21 => '21',22 => '22',23 => '23'}
                 graphgenerate(xlabels,powerdata,'Hour','Power Usage[MW]','PowerUsageGraph'+dateyear.to_s+'-'+datemonth.to_s+'-'+dateday.to_s)
         end
@@ -82,11 +97,11 @@ post '/todengraphdaylingr' do
                                 datemon=(Date.today-dateparam).mon
                                 dateday=(Date.today-dateparam).mday
                                 if(dateparam>0) then
-                                        graphdatetime=pastfileanalysis(dateyear,datemon,dateday)
+                                        graphdatetime=pastfileanalysis(dateyear,datemon,dateday,text.index("diff"))
                                         return 'http://v157-7-153-173.z1d1.static.cnode.jp/tmpfigure/TodenGraphDayLingr/PowerUsageGraph'+dateyear.to_s+'-'+datemon.to_s+'-'+dateday.to_s+'.png'
                                 else
 					filename = 'NowPowerUsageGraph'+[*1..9, *'A'..'Z', *'a'..'z'].sample(8).join
-                                        graphdatetime=todayfileanalysis(filename)
+                                        graphdatetime=todayfileanalysis(filename,text.index('diff'))
                                         return "http://v157-7-153-173.z1d1.static.cnode.jp/tmpfigure/TodenGraphDayLingr/"+filename+".png"
                                 end
 			else
